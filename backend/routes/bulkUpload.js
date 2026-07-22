46d2c671ac38d1cfbe3da429c76bbd7eec5ebd11
Kavyansh <kavyansh036@gmail.com>
2026-07-22 22:20:15 +0530

    Fix Product model filename case

--- backend/routes/bulkUpload.js
+++ backend/routes/bulkUpload.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const XLSX = require('xlsx');
-const Product = require('../models/Product');
+const Product = require('../models/product');
 const router = express.Router();
 
 // Configure multer for file upload
