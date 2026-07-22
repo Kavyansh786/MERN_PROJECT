a1c6fd0254d28187fe3e22f1f061ce1852958d47
Kavyansh <kavyansh036@gmail.com>
2026-07-22 23:00:55 +0530

    Fix mongo error case

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/jwellerydb');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('Connection error:', err);
