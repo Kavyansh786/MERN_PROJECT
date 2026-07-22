7a1b3d5c5172038e484a265f50360ec07a22228b
Kavyansh <kavyansh036@gmail.com>
2026-07-22 22:13:58 +0530

    Fix Cart model filename case

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Cart = require('../models/Cart');
+const Cart = require('../models/cart');
 const Product = require('../models/product');
 const User = require('../models/user');
 const mongoose = require('mongoose');
